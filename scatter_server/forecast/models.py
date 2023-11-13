b0a43fd991f0b15e48f434bcb46987298c38470f
Lee <ehgus8621@gmail.com>
2023-11-14 08:06:14 +0900

    .

--- scatter_server/forecast/models.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.db import models
-from django.conf import settings
-# Create your models here.
-
-class ImageFile(models.Model):
-    file_name = models.CharField(max_length = 50)
-    image = models.ImageField(default = os.path.join(settings.MEDIA_ROOT, "/forecast_image"))
